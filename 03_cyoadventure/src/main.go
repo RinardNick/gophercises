23c2a97da2e03e2120a16609db12ea77ebc01806
Nick Rinard <nar.scm@gmail.com>
2020-05-17 10:42:29 -0600

    fixed can't find package with folder for package

---
+++ 03_cyoadventure/src/main.go
@@ -0,0 +1,35 @@
+package main
+
+// reading in files in go https://appdividend.com/2019/12/19/how-to-read-files-in-golang-go-file-read-example/
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	var storyPath string = "storyTemplate.json"
+	storyBytes, err := readWholeFile(string(storyPath))
+	if err != nil {
+		log.Println("Failed to read in file: ", err)
+	}
+
+	var story interface{}
+	err = json.Unmarshal(storyBytes, &story)
+	if err != nil {
+		log.Println("Failed to Unmarshall JSON: ", err)
+	}
+
+	fmt.Println(story)
+}
+
+func readWholeFile(filepath string) ([]byte, error) {
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		fmt.Println("File reading error: ", err)
+		return nil, err
+	}
+	return data, nil
+}
